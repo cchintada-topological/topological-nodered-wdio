6085956f6a5c473df9235c4be92a2f2cfc2c51e0
cchintada-topological <79113644+cchintada-topological@users.noreply.github.com>
2025-10-23 15:12:50 +0000

    Completed Multip select for all

--- src/dropdown-action.js
+++ src/dropdown-action.js
@@ -1,6 +1,6 @@
 const common = require('./wdio-common')
 
-module.exports = function(RED) {
+module.exports = function (RED) {
   function dropdownAction(config) {
     RED.nodes.createNode(this, config)
     const node = this
@@ -8,15 +8,48 @@ module.exports = function(RED) {
 
     node.on('input', async (msg) => {
       try {
-        let locateUsing = config.locateUsing || msg.locateUsing
-        let locateValue = config.locateValue || msg.locateValue
+        common.clearStatus(node);
+        let multiple = config.locateType || msg.locateType;
+        let locateValues = config.locateValues || msg.locateValues;
+        let locateUsing = config.locateUsing || msg.locateUsing;
+        let locateValue = config.locateValue || msg.locateValue;
+        let element = null;
+        node.log = "";
 
         let browser = await common.getBrowser(node.context())
-        let locator = await common.getLocator(
-          locateUsing,
-          locateValue
-        )
-        let element = await browser.$(locator)
+        if(!multiple){
+          let locator = await common.getLocator(
+            locateUsing,
+            locateValue
+          )
+          element = await browser.$(locator)
+          if (!element) {
+            throw new Error(`Element not found using ${locateUsing}: ${locateValue}`);
+          }
+        }
+        else{
+          for(let i = 0; i <locateValues.length; i++){
+            const { using, value } = locateValues[i];
+            let locator = await common.getLocator(
+            locateUsing,
+            locateValue
+            )
+            element = await browser.$(locator)
+            if (element) {
+              node.log += `Attempt ${i + 1}: Element found using ${using}: ${value}\n`;
+              locateUsing = using
+              locateValue = value
+              break;
+            }
+            else {
+              node.log += `Attempt ${i + 1}: Element not found using ${using}: ${value}\n`;
+              node.warn(`Element not found using ${using}: ${value}`);
+            }
+          }
+          if (!element) {
+            throw new Error(`Element not found using all selector values`);
+          }
+        }
 
         let text = config.text || msg.text
         let attribute = config.attribute || msg.attribute
@@ -24,16 +57,16 @@ module.exports = function(RED) {
         let value = config.value || msg.value
 
         if (config.action === 'selectByAttr') {
-          node.log = `Select the dropdown value using Attribute: ${attribute} with Value: ${value}.`
+          node.log += `Select the dropdown value using Attribute: ${attribute} with Value: ${value}.`
           await element.selectByAttribute(attribute, value)
         } else if (config.action === 'selectByIndex') {
-          node.log = `Select the dropdown value using Index: ${index}.`
+          node.log += `Select the dropdown value using Index: ${index}.`
           await element.selectByIndex(parseInt(index))
         } else if (config.action === 'selectByText') {
-          node.log = `Select the dropdown value using Visible text: ${text}.`
+          node.log += `Select the dropdown value using Visible text: ${text}.`
           await element.selectByVisibleText(text)
         } else if (config.action === 'getValue') {
-          node.log = 'Get selected drop down value.'
+          node.log += 'Get selected drop down value.'
           msg.payload = await element.getValue()
         }
         await common.log(node)
